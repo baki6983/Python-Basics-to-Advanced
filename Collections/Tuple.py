3570e484aa7104ceb2b06adb220feba3b12bdda7
unknown <balakrishnasuggu@outlook.com>
2018-09-12 21:50:35 +0200

    tuples example

--- Collections/Tuple.py
+++ Collections/Tuple.py
@@ -7,7 +7,7 @@ print(fruitsTuples[1])
 
 # if you try to assign value to fruitsTuples[1] , it will change because its Unchangeable
 
-#  With DEL method you can completely TUPLE
+#  With DEL method you can completely List , but you cant item in the list
 
 
 for i in fruitsTuples:
